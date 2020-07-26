37f42cefc58daeb81905bb46aaa03bb1b77fba80
Paul Houghton <paul.houghton@gmail.com>
2020-07-26 08:40:57 +0100

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Paul Houghton on 26/07/2020.
+//  Copyright © 2020 Paul Houghton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
